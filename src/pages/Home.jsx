0f34154a0d32c4ad4f237df6f609f2ad166b3310
Pushkaraj <pushkaraj4444@gmail.com>
2023-12-13 17:45:55 +0530

    Navbar, Carousel and contacts page draft

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SimpleCarousel from '../components/SimpleCarousel'
+import Contact from '../components/Contact'
 
 const Home = () => {
   return (
     <div>
       <SimpleCarousel/>
-      
+      <Contact/>
     </div>
   )
 }

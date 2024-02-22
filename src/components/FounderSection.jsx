6ae3fe3263fe11256eb50cde2cda8d785e8f61b0
Yogiraj Patil <yogiraj10033921@gmail.com>
2024-02-22 17:03:35 +0530

    linked in added

--- src/components/FounderSection.jsx
+++ src/components/FounderSection.jsx
@@ -13,7 +13,7 @@ const FounderSection = ({}) => {
           description: "Sunil Goenka specializes in business communication services, encompassing PR Branding, Content Strategy, and Customer Connect Initiatives, complemented by his role as a Business Development advisor. With a notable portfolio, he has successfully served over 200 brands within his professional domain. Sunil also holds the esteemed position of curator for the Mumbai Entrepreneurs Award and Kolkata Entrepreneurs Award.The recognition and respect these awards enjoy stem from Sunil's commitment to an organic and meticulous awardee selection process.The recognition and respect these awards enjoy stem from Sunil's commitment to an organic and meticulous awardee selection process.",
           imageUrl: 'https://yugmav.in/images/SunilGoenka1.png',
           linkedin: 'https://www.linkedin.com/in/sunil-goenka-0283089/',
-          twitter: 'https://twitter.com/johndoe',
+          // twitter: 'https://twitter.com/johndoe',
         },
     
         {
@@ -22,7 +22,7 @@ const FounderSection = ({}) => {
           description: 'An accomplished Finance and Accounts Techno professional, characterized by a positive outlook, creative mindset, and proficient analytical abilities. With an extensive network in the business landscape, particularly within Financial Institutions, External Rating Analysts, and Valuers of property, projects, and companies, I bring over 30 years of hands-on experience in diverse service profiles across multiple companies since 1992.',
           imageUrl: 'https://yugmav.in/images/ShubankarMandal1_1.png',
           linkedin: 'https://www.linkedin.com/in/johndoe/',
-          twitter: 'https://twitter.com/johndoe',
+          // twitter: 'https://twitter.com/johndoe',
         },
     
         {
@@ -30,8 +30,8 @@ const FounderSection = ({}) => {
           post: 'Co-Founder & COO',
           description: 'A seasoned Customer Service Manager with a proven track record of achieving impactful results, spanning various sectors such as B2B, e-commerce, and BPO. Proficient in customer service, adept at aligning delivery processes to meet customer requirements and optimize collections. Possesses robust skills in communication, operations, strategic planning, and team building. Holds a management degree from XLRI, complemented by a certification in performance operations management from XLRI-VIL Programs',
           imageUrl: 'https://yugmav.in/images/devyani_1.png',
-          linkedin: 'https://www.linkedin.com/in/johndoe/',
-          twitter: 'https://twitter.com/johndoe',
+          linkedin: 'https://www.linkedin.com/in/devyani-kembral-a9b03128/',
+          // twitter: 'https://twitter.com/johndoe',
         },
         
       ];

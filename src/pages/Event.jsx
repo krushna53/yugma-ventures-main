6c3376a8085943b08a59072696a60f8ccb314871
MosinInamdar <mosininamdar18@gmail.com>
2024-04-08 21:10:30 +0530

    event detail css

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,59 +1,32 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { FaLightbulb, FaUserFriends, FaChalkboardTeacher } from 'react-icons/fa'; // Import icons from react-icons
-import './Event.css';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  FaLightbulb,
+  FaUserFriends,
+  FaChalkboardTeacher,
+} from "react-icons/fa"; // Import icons from react-icons
+import "./Event.css";
 // import MSME from '../images/MSMEEvent.png'
-import eventData from '../eventsData.json';
+import eventData from "../eventsData.json";
 
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
 const Event = () => {
-  // const eventDetails = [
-  //   {
-  //     eventName: 'elevate-msme-sector-with-ai-powered-innovation',
-  //     name: 'ELEVATE MSME SECTOR WITH AI-POWERED INNOVATION',
-  //     imageUrl: MSME,
-  //     date: '16th Dec',
-  //     venue: 'Mumbai Press Club, Glass House, Mahapalika Marg, Azad Maidan, Fort, Mumbai, Maharashtra 400 001',
-  //     time: '2:30 pm - 5:30 pm',
-  //     introTitle: 'WELCOME TO THE FUTURE OF BUSINESS: ELEVATE MSME SECTOR WITH AI-POWERED INNOVATION!',
-  //     introDesc: "Join us on this transformative journey and be part of the AI-powered revolution that is reshaping the MSME sector. Elevate your business, empower your team, and embrace the future!For sponsorship opportunities, inquiries, or further information, contact us at contact link in the menu above.Don't miss out on the chance to be at the forefront of innovation. See you there! #ElevateMSME #AIPoweredInnovation",
-  //     whyAttend: ['Explore Limitless Potential : Gain insights into the vast opportunities AI presents for MSMEs.', 'Networking Opportunities : Connect with like-minded professionals, industry experts, and thought leaders.', 'Expert Speakers : Learn from industry experts, successful entrepreneurs, and AI pioneers.'],
-  //     whoShouldAttend: ['Entrepreneurs and Business Owners', 'Technology Enthusiasts', 'MSME Executives and Decision-Makers','AI Developers and Innovators','Government Representatives and Policymakers'],
-  //     registrationLinkAttendee: 'https://docs.google.com/forms/d/e/1FAIpQLScrFUA2S6HrvBjNdMCm69y9GjAZV-KouPikwJUVNK84BYveYw/viewform',
-  //     registrationLinkSpeaker: 'https://docs.google.com/forms/d/e/1FAIpQLSdQa2o6qgyiVPgqMEBgDF4LVC6tkbz3AMx_63dow3Lyju5YHA/viewform',
-  //   },
-  //   {
-  //     name: 'Understanding of Web Development',
-  //     imageUrl: 'https://example.com/event1-image.jpg',
-  //     date: 'Jan 1',
-  //     venue: 'Tech Hall',
-  //     time: 'Pune',
-  //     introTitle: 'WELCOME TO THE WORLD OF DATA, APPS AND AI COLLABORATION',
-  //     introDesc: 'Snowflake is coming home to San Francisco. Join us at Snowflake Summit 2024 to explore all the cutting-edge innovation the Data Cloud has to offer. Discover the latest in AI, genAI, Apache Iceberg, streaming, privacy-preserving collaboration, flexible programmability, application development and much more.',
-  //     whyAttend: ['Gain insights into the vast opportunities AI presents for MSMEs.', 'Connect with like-minded professionals, industry experts, and thought leaders.', 'Learn from industry experts, successful entrepreneurs, and AI pioneers.'],
-  //     whoShouldAttend: ['Developers', 'Blockchain enthusiasts', 'Tech enthusiasts'],
-  //     registrationLinkAttendee: 'https://example.com/register-attendee',
-  //     registrationLinkSpeaker: 'https://example.com/register-speaker',
-  //   },
-  // ];
-
-  
-
   // Scroll to top when the component mounts
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }, []); // Empty dependency array ensures this effect runs only once when the component mounts
-
 
   // const { index } = useParams();
   // const event = eventDetails[index];
 
   const { eventPathName } = useParams();
-  const event = eventData.eventDetails.find((e) => e.eventName === eventPathName);
+  const event = eventData.eventDetails.find(
+    (e) => e.eventName === eventPathName
+  );
   const imageUrl = event.imageUrl;
   // console.log(imageUrl)
 
@@ -61,68 +34,82 @@ const Event = () => {
     return <div>Error: Event not found</div>;
   }
 
-
   const handleRegistration = (type) => {
-    const registrationLink = type === 'attendee' ? event.registrationLinkAttendee : event.registrationLinkSpeaker;
-    window.open(registrationLink, '_blank');
+    const registrationLink =
+      type === "attendee"
+        ? event.registrationLinkAttendee
+        : event.registrationLinkSpeaker;
+    window.open(registrationLink, "_blank");
   };
 
- 
   return (
     <>
-    <div className='event-detail-block'>
-      <div className='event-img-detail'>
-        <img src={imageUrl} alt={event.name} className="event-background" />
+      <div className="event-detail-block">
+        <div className="event-img-detail">
+          <img src={imageUrl} alt={event.name} className="event-background" />
 
-        <div className="event-details">
-          <div className='event-name'>
-            <h1>{event.name}</h1>
+          <div className="event-details">
+            <div className="event-name">
+              <h1>{event.name}</h1>
+            </div>
+            <div className="event-date-venue">
+              <h2>
+                {event.venue} | {event.date} | {event.time}
+              </h2>
+            </div>
+            <div className="registration-options">
+              <button onClick={() => handleRegistration("attendee")}>
+                Register as Attendee
+              </button>
+              <button onClick={() => handleRegistration("speaker")}>
+                Register as Speaker
+              </button>
+            </div>
           </div>
-          <div className='event-date-venue'>
-              <h2>{event.venue}   | {event.date}   |   {event.time}</h2>
-          </div>
-          <div className="registration-options">
-            <button onClick={() => handleRegistration('attendee')}>Register as Attendee</button>
-            <button onClick={() => handleRegistration('speaker')}>Register as Speaker</button>
+        </div>
+        <div className="event-description-container">
+          <div className="event-description">
+            <div className="event-intro">
+              <div className="event-intro-title">
+                <h1>{event.introTitle}</h1>
+              </div>
+              <div className="event-intro-desc">
+                <p>{event.introDesc}</p>
+              </div>
+            </div>
+            <div className="event-who-why">
+              <div className="event-why">
+                <h1>Why Attend?</h1>
+                <ul>
+                  <li>
+                    <FaLightbulb style={{ fontSize: "30px" }} />{" "}
+                    {event.whyAttend[0]}
+                  </li>
+                  <li>
+                    <FaUserFriends style={{ fontSize: "30px" }} />{" "}
+                    {event.whyAttend[1]}
+                  </li>
+                  <li>
+                    <FaChalkboardTeacher style={{ fontSize: "30px" }} />{" "}
+                    {event.whyAttend[2]}
+                  </li>
+                </ul>
+              </div>
+
+              <div className="event-who">
+                <h1>Who Should Attend?</h1>
+                <ul>
+                  {event.whoShouldAttend.map((point, i) => (
+                    <li key={i}>{point}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="event-description-container">
-      <div className="event-description">
-          <div className='event-intro'>
-            <div className='event-intro-title'>
-              <h1>{event.introTitle}</h1>
-            </div>
-            <div className='event-intro-desc'>
-              <p>{event.introDesc}</p>
-            </div>
-          </div>
-          <div className='event-who-why'>
-            <div className='event-why'>
-              <h1>Why Attend?</h1>
-              <ul>
-                <li><FaLightbulb style={{ fontSize: '30px' }} /> {event.whyAttend[0]}</li>
-                <li><FaUserFriends style={{ fontSize: '30px' }} /> {event.whyAttend[1]}</li>
-                <li><FaChalkboardTeacher style={{ fontSize: '30px'}}/> {event.whyAttend[2]}</li>
-              </ul>
-            </div>
-
-            <div className='event-who'>
-              <h1>Who Should Attend?</h1>
-              <ul>
-                {event.whoShouldAttend.map((point, i) => (
-                  <li key={i}>{point}</li>
-                ))}
-              </ul>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      
-    </div>
-    <div className="footer" id="footer">
-        <Footer/>
+      <div className="footer" id="footer">
+        <Footer />
       </div>
     </>
   );
--- src/pages/EventDetail.jsx
+++ src/pages/EventDetail.jsx
@@ -1,29 +1,33 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { FaLightbulb, FaUserFriends, FaChalkboardTeacher } from 'react-icons/fa'; // Import icons from react-icons
-import './Event.css';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  FaLightbulb,
+  FaUserFriends,
+  FaChalkboardTeacher,
+} from "react-icons/fa"; // Import icons from react-icons
+// import "./Event.css";
 // import MSME from '../images/MSMEEvent.png'
-import eventData from '../eventsData.json';
+import eventData from "../eventsData.json";
+import "./eventdetails.css";
 
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
 const EventDetail = () => {
-  
-
   // Scroll to top when the component mounts
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }, []); // Empty dependency array ensures this effect runs only once when the component mounts
-
 
   // const { index } = useParams();
   // const event = eventDetails[index];
 
   const { eventPathName } = useParams();
-  const event = eventData.eventDetails.find((e) => e.eventName === eventPathName);
+  const event = eventData.eventDetails.find(
+    (e) => e.eventName === eventPathName
+  );
   const imageUrl = event.imageUrl;
   // console.log(imageUrl)
 
@@ -31,66 +35,78 @@ const EventDetail = () => {
     return <div>Error: Event not found</div>;
   }
 
-
   const handleRegistration = (type) => {
-    const registrationLink = type === 'attendee' ? event.registrationLinkAttendee : event.registrationLinkSpeaker;
-    window.open(registrationLink, '_blank');
+    const registrationLink =
+      type === "attendee"
+        ? event.registrationLinkAttendee
+        : event.registrationLinkSpeaker;
+    window.open(registrationLink, "_blank");
   };
 
- 
   return (
     <>
-    <div className='event-detail-block'>
-      <div className='event-img-detail'>
-        <img src={imageUrl} alt={event.name} className="event-background" />
+      <div className="event-detail-block">
+        <div className="event-img-detail">
+          <img src={imageUrl} alt={event.name} className="event-background" />
 
-        <div className="event-details">
-          <div className='event-name'>
-            <h1>{event.name}</h1>
-          </div>
-          <div className='event-date-venue'>
-              <h2>{event.venue}   | {event.date}   |   {event.time}</h2>
+          <div className="event-details">
+            <div className="event-name">
+              <h1>{event.name}</h1>
+            </div>
+            <div className="event-date-venue">
+              <h2>
+                {event.venue} | {event.date} | {event.time}
+              </h2>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="event-description-container">
-      <div className="event-description">
-          <div className='event-intro'>
-            <div className='event-intro-title'>
-              <h1>{event.introTitle}</h1>
+        <div className="event-description-container">
+          <div className="event-description">
+            <div className="event-intro">
+              <div className="event-intro-title">
+                <h1>{event.introTitle}</h1>
+              </div>
+              <div className="event-intro-desc">
+                <p>{event.introDesc}</p>
+              </div>
             </div>
-            <div className='event-intro-desc'>
-              <p>{event.introDesc}</p>
-            </div>
-          </div>
-          <div className='event-video'>
-                {/* Dummy video content */}
-                <iframe 
+            <div className="event-video">
+              {/* Dummy video content */}
+              <iframe
                 title="Dummy Video"
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
-                ></iframe>
-          </div>
-          <div className='event-desc-img'>
-                <div className='event-pastdesc'>
+              ></iframe>
+            </div>
+            <div className="event-desc-img">
+              <div className="event-pastdesc">
                 {/* Dummy description for past events */}
-                <p>This is a dummy description for past events. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className='event-pastimg'>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Suscipit omnis, eligendi itaque nostrum voluptas tempore
+                  consequatur incidunt voluptatibus iure fugit dolorem vitae
+                  voluptatum similique? Commodi odio hic totam nihil quaerat!
+                  Quia quam quaerat officiis fuga id eveniet commodi rerum autem
+                  labore tempora deserunt ratione dolore architecto, aut,
+                  molestiae, illo recusandae sed dolorem maxime saepe ea laborum
+                  ipsum. Accusamus, deleniti est.
+                </p>
+              </div>
+              <div className="event-pastimg">
                 {/* Dummy image for past events */}
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAUE-L3jF7fybmubhDWkz1-pShYyAUkVHk06kVojiivw&s" alt="Past Event" />
-                </div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAUE-L3jF7fybmubhDWkz1-pShYyAUkVHk06kVojiivw&s"
+                  alt="Past Event"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      
-    </div>
-    <div className="footer" id="footer">
-        <Footer/>
+      <div className="footer" id="footer">
+        <Footer />
       </div>
     </>
   );
